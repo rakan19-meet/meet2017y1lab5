899fcbee032b4c11dababa19d74b45222bb7e97a
rakan19-meet <rakan19@meet.mit.edu>
2017-07-30 13:51:03 +0300

    Stopping here

---
+++ add_numbers.py
@@ -0,0 +1,16 @@
+
+
+
+
+#####################################################
+#writing the function
+########################################################
+def add_numbers (start, end):
+    # this function should add
+    # all the numbers from start
+    # to end, including both
+    # write your code here
+    c = 0
+    for n in range(start, end + 1):
+        c = c + n
+    return c
---
+++ fun2.py
@@ -0,0 +1,5 @@
+def draw_1d(n, shape):
+    print(n*shape)
+    
+    
+    
